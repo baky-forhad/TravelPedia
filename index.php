06024d67df82929ec7b4b9cbe5eb117129cd27b3
DipTo Zaman <iamdipto7@gmail.com>
2017-10-07 03:29:20 +0600

    tested css design

---
+++ index.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+    <link rel="stylesheet" href="https://portal.aiub.edu/Content/base?v=t74ME33IcTPLNcv-vSTDlqhfmWLRwM6BjhGDNppmPpo1">
+    <link rel="stylesheet" href="https://portal.aiub.edu/Content/dist/drunkenparrot/css/drunken-parrot.css">
+
+    <style media="screen">
+    .login-form {
+  width: 75%;
+  max-width: 350px;
+  margin: auto;
+  position: absolute;
+  left: 0;
+  right: 0;
+  padding: 20px;
+  border: 1px solid #dddddd;
+  background-color: #ffffff;
+}
+
+.side-link {
+    position: absolute;
+    right: 10px;
+    top: 0;
+    z-index: 10;
+    font-size: 12px;
+    line-height: 45px;
+}
+    </style>
+
+  </head>
+  <body style="background-color: rgb(227, 231, 232)">
+
+    <div >
+
+      <div class="login-form" style="top: 172px;">
+
+
+        <form class="" action="index.html" method="post">
+
+          <h3>Log in to TravelPedia!!!</h3>
+          <small>I'm the one who travels</small>
+          <table>
+            <tr>
+              <td>Enter Username or Email</td>
+            </tr>
+
+            <tr>
+              <td>
+                <input type="text" name="" value="" placeholder="username or email">
+              </td>
+            </tr>
+
+            <tr>
+              <td>Enter Password</td>
+            </tr>
+
+            <tr>
+              <td>
+                <div class="">
+                  <input type="password" name="" value="" placeholder="password">
+                  <a href="#" class="side-link"><small>forget password?</small></a>
+                </div>
+
+              </td>
+            </tr>
+
+            <tr>
+              <td>
+                <input type="submit" name="" value="Log in" style="text-align:center,text-weigh:bold">
+              </td>
+            </tr>
+
+
+            <tr>
+              <td style="text-align:left">
+                <a href="#"><small>forget password?</small></a>
+              </td>
+            </tr>
+
+          </table>
+
+
+
+
+
+        </form>
+
+              </div>
+
+</div>
+
+
+
+
+  </body>
+</html>
