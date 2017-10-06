d0062bcda7d938fa2eefb06af704d0e6ac72ced6
Baki Forhad <bakiforhad11@gmail.com>
2017-10-07 04:58:25 +0600

    css added in login and signup

--- login.php
+++ login.php
@@ -2,67 +2,123 @@
 <html>
   <head>
     <meta charset="utf-8">
-    <title>Log in to TravelPedia</title>
+    <title>log in</title>
+
+    <style media="screen">
+    .login-form
+    {
+        width: 75%;
+        max-width: 350px;
+        margin: auto;
+        position: absolute;
+        left: 0;
+        right: 0;
+        padding: 20px;
+        border: 1px solid #dddddd;
+        background-color: #ffffff;
+    }
+    #join-link
+    {
+        text-decoration: none;
+        text-decoration-color: red;
+    }
+
+    input
+    {
+        height: 30px;
+        width: 300px;
+        margin-top: 7px;
+        margin-bottom: 7px;
+        font-size: 12pt;
+        padding-left: 6px;
+
+
+    }
+    #btn-login
+    {
+        width: 310px;
+        font-weight: bold;
+    }
+
+
+
+
+    </style>
+
   </head>
-  <body>
-    <div class="container" id="loginpage-container" style="top: 40px;">
-      <div class="loginpanel" id="log in panel" align="center" width="100%"  >
-        <form class="" action="loginvalidate.php" method="post">
-          <h3>
-              Log in to TravelPedia!!!<br>
-              <small>I'm the one who travels</small>
-          </h3>
+  <body style="background-color: rgb(227, 231, 232)">
+
+    <div>
+
+  <div class="login-form" style="top: 172px;">
+
+      <h2 align="center" style="margin:0px">
+          Log in to TravelPedia!!!
+          <small>
+              <h6  style="margin:0px" >I'm the one who travels </h6>
+          </small>
+      </h2>
+      <hr>
+      <br>
 
 
-          <table  >
+        <form class="" action="index.php" method="post">
+
+
+
+
+
+
+
+          <table align="center">
+
             <tr>
-              <td>Enter Username or Email</td>
+              <td>
+                <div>
+                    <input type="text" name="" value="" placeholder="username or email">
+                </div>
+              </td>
+
             </tr>
 
             <tr>
               <td>
-                <input type="text" name="" value="" placeholder="username or email">
-              </td>
-            </tr>
+                  <input type="password" name="" value="" placeholder="password">
 
-            <tr>
-              <td>Enter Password</td>
+              </td>
             </tr>
 
             <tr>
               <td>
-                <input type="password" name="" value="" placeholder="password">
+                <input id="btn-login" type="submit" name="" value="Log in">
               </td>
             </tr>
+
+
             <tr>
-              <td style="text-align:left">
+              <td style="text-align:right">
                 <a href="#"><small>forget password?</small></a>
               </td>
             </tr>
 
-            <tr>
-              <td width="100%">
-                 <input type="submit" name="" value="Log in" >
-              </td>
-            </tr>
-
-
-
-
           </table>
 
-
-
-
         </form>
+        <br><hr>
+
+        <p align="center">
+            New to TravelPedia?
+            <a id="join-link" href="signup.php"><b style="color:red">Join Now!<b></a>
+        </p>
+
+        <hr>
 
 
-
-      </div>
 
 
     </div>
 
+</div>
 
 
 
--- signup.php
+++ signup.php
@@ -3,59 +3,114 @@
     <head>
         <meta charset="utf-8">
         <title>Sign up</title>
+        <style media="screen">
+
+            .signup-form
+             {
+                width: 75%;
+                max-width: 350px;
+                margin: auto;
+                position: absolute;
+                left: 0;
+                right: 0;
+                padding: 20px;
+                border: 1px solid #dddddd;
+                background-color: #ffffff;
+            }
+
+            input
+            {
+                height: 30px;
+                width: 300px;
+                margin-top: 7px;
+                margin-bottom: 7px;
+                font-size: 12pt;
+                padding-left: 6px;
+
+
+            }
+            #btn-signup
+            {
+                width: 309px;
+                font-weight: bold;
+            }
+
+            #login-link
+            {
+                text-decoration: none;
+                text-decoration-color: red;
+            }
+
+        </style>
+
+
+
     </head>
-    <body>
-        <h3>Sign up page</h3>
+    <body style="background-color: rgb(227, 231, 232)">
 
-        <div class="">
-            <div class="">
+        <div class="signup-form" style="top:100px" >
+
+
+                    <h2 align="center" style="margin:0px">
+                        Sign up page
+                        <small>
+                            <h6  style="margin:0px" >
+                                ready to explore
+                            </h6>
+                        </small>
+                    </h2>
+                    <hr>
+                    <br>
                 <form class="" action="home.php" method="post">
-                    <tr>
-                        <td>Email</td>
-                    </tr>
+                    <table align="center" >
 
-                    <tr>
-                        <td>
-                            <input type="email" name="" value="" placeholder="example@mail.com">
-                        </td>
-                    </tr>
+                        <tr>
+                            <td>
+                                <input type="email" name="" value="" placeholder="example@mail.com">
+                            </td>
+                        </tr>
 
-                    <tr>
-                        <td>Username</td>
-                    </tr>
-
-                    <tr>
-                        <td>
-                            <input type="text" name="" value="" placeholder="jon_snow">
-                        </td>
-                    </tr>
-
-                    <tr>
-                        <td>Password</td>
-                    </tr>
-
-                    <tr>
-                        <td>
-                            <input type="password" name="" value="" placeholder="password">
-                        </td>
-                    </tr>
-
-                    <tr>
-                        <td>Confirm Password</td>
-                    </tr>
-
-                    <tr>
-                        <td>
-                            <input type="password" name="" value="" placeholder="">
-                        </td>
-                    </tr>
+                        <tr>
+                            <td>
+                                <input type="text" name="" value="" placeholder="jon_snow">
+                            </td>
+                        </tr>
 
 
+                        <tr>
+                            <td>
+                                <input type="password" name="" value="" placeholder="password">
+                            </td>
+                        </tr>
 
 
+                        <tr>
+                            <td>
+                                <input type="password" name="" value="" placeholder="confirm password">
+                            </td>
+                        </tr>
+
+                        <tr>
+                            <td>
+                                <input id="btn-signup" align="center"  type="submit" name="" value="Sign up">
+                            </td>
+                        </tr>
+                    </table>
                 </form>
+                <br><hr>
+
+                <p align="center">
+                    Already a member?
+                    <a id="login-link" href="login.php"><b style="color:red">Sign in Now!<b></a>
+                </p>
+
+                <hr>
+
+
+
+
             </div>
-        </div>
+
 
 
 
