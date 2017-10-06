f69e92c8ab1712d9a6ce23428a759150becb63f4
Baki Forhad <bakiforhad11@gmail.com>
2017-10-06 23:38:42 +0600

    log in page with awesome css

---
+++ login.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Log in to TravelPedia</title>
+  </head>
+  <body>
+    <div class="container" id="loginpage-container">
+      <div class="loginpanel" id="log in panel" align="center">
+        <form class="" action="index.html" method="post">
+
+          <h3>Log in to TravelPedia!!!</h3>
+          <small>I'm the one who travels</small>
+          <table>
+            <tr>
+              <td>Enter Username or Email</td>
+            </tr>
+
+            <tr>
+              <td>
+                <input type="text" name="" value="" placeholder="username or email">
+              </td>
+            </tr>
+
+            <tr>
+              <td>Enter Password</td>
+            </tr>
+
+            <tr>
+              <td>
+                <input type="password" name="" value="" placeholder="password">
+              </td>
+            </tr>
+
+            <tr>
+              <td>
+                <input type="submit" name="" value="Log in" style="text-align:center,text-weigh:bold">
+              </td>
+            </tr>
+
+
+            <tr>
+              <td style="text-align:left">
+                <a href="#"><small>forget password?</small></a>
+              </td>
+            </tr>
+
+          </table>
+
+
+
+
+
+        </form>
+
+
+
+      </div>
+
+      <footer>travelpedia</footer>
+
+    </div>
+
+
+
+
+
+  </body>
+</html>
