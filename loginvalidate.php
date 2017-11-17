0b5f299a0655d242a1fd6270011f9e85cd5e908c
Baki Forhad <bakiforhad11@gmail.com>
2017-11-18 03:23:56 +0600

    function.php added

--- loginvalidate.php
+++ loginvalidate.php
@@ -5,14 +5,6 @@
         <title></title>
     </head>
     <body>
-<<<<<<< HEAD
-        <?php
-        session_start();
-        $_SESSION["IsloggedIn"]=True;
-        header("Location:createpost.php");
-        ?>
-=======
->>>>>>> ae31e5d435e04ee8550b74143840a8aded1a5bed
 
     <?php
       session_start();
